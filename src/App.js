016bdd26d876dc8da6c9954b4b72475f15f54703
Bahadir Colak <bahadircolak34@gmail.com>
2022-04-25 21:55:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <>   
+      <Router>
+         <Sidebar />
+         <Routes>
+           <Route  path='/'/>
+         </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as FaIcons from "react-icons/fa";
+import {Link} from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <>
+      <div className='Sidebar'>
+        <h1>fffefe</h1>
+      </div>  
+    </>
+  )
+}
+
+export default Sidebar
